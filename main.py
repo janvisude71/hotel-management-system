72bd0c39e68740b94f3997a3d961207ed07ae0c5
Janavi  Sude <janvisude71@gmail.com>
2025-12-20 21:48:22 +0530

    Added basic menu logic

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("GitHub project setup successful")
-print("janavi")
+print("===== Hotel Management System =====")
+
+print("1. Book Room")
+print("2. Exit")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    name = input("Enter customer name: ")
+    room = input("Enter room number: ")
+    print("Room booked successfully!")
+elif choice == "2":
+    print("Thank you for using the system")
+else:
+    print("Invalid choice")
